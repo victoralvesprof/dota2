4db43ca8e9d7ff662e98f7ac6cb6fdce5e9d574e
victoralvesprof <victoralves.prof@gmail.com>
2024-04-13 14:46:39 -0300

    feat: add lib layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { LayoutModule } from '@dota2/layout';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, LayoutModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
